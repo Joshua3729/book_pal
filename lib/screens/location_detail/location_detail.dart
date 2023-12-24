8200b7077fdd947f456ccd20ef12d984a61018e1
Joshua Khumalo <3729344@myuwc.ac.za>
2023-12-24 22:34:33 +0200

    Added location detail text section

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,3 +1,4 @@
+import 'package:book_pal/screens/location_detail/text_section.dart';
 import 'package:flutter/material.dart';
 
 class LocationDetail extends StatelessWidget {
@@ -7,25 +8,13 @@ class LocationDetail extends StatelessWidget {
         appBar: AppBar(
           title: Text("Welcome"),
         ),
-        body: Column(children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.red,
-            ),
-            child: Text('hi'),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.green,
-            ),
-            child: Text('hi'),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text('hi'),
-          )
-        ]));
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextSection(Colors.red),
+              TextSection(Colors.green),
+              TextSection(Colors.blue)
+            ]));
   }
 }
