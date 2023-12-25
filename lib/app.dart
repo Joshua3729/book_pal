c4bb03f3e884a15a38dd79252d2251b6f4bda407
Joshua Khumalo <3729344@myuwc.ac.za>
2023-12-26 01:15:26 +0200

    deprecated the app

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,1 @@
-import 'package:book_pal/screens/location_detail/location_detail.dart';
-import 'package:flutter/material.dart';
-import "style.dart";
 
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: LocationDetail(),
-        theme: ThemeData(
-            appBarTheme: AppBarTheme(
-                titleTextStyle:
-                    TextTheme(titleLarge: AppBarTextStyle).headline6),
-            primaryColor: Color.fromRGBO(0, 0, 0, 0.54)));
-  }
-}
