c07bd3e5e33ac1252d14cd446f5a18d0c469e2ec
Joshua Khumalo <3729344@myuwc.ac.za>
2023-12-25 23:57:54 +0200

    added dart styles

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:book_pal/screens/location_detail/location_detail.dart';
 import 'package:flutter/material.dart';
-import "style.dart"
+import "style.dart";
 
 class App extends StatelessWidget {
   @override
@@ -8,7 +8,9 @@ class App extends StatelessWidget {
     return MaterialApp(
         home: LocationDetail(),
         theme: ThemeData(
-            appBarTheme:
-                AppBarTheme(textTheme: TextTheme(title: AppBarTextStyle))));
+            appBarTheme: AppBarTheme(
+                titleTextStyle:
+                    TextTheme(titleLarge: AppBarTextStyle).headline6),
+            primaryColor: Color.fromRGBO(0, 0, 0, 0.54)));
   }
 }
