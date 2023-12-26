01a7e9a0345c50328782361db56dca5c99c0a536
Joshua Khumalo <3729344@myuwc.ac.za>
2023-12-26 23:06:02 +0200

    using expanded widget

--- lib/app.dart
+++ lib/app.dart
@@ -6,47 +6,59 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            backgroundColor: Colors.deepPurple[200],
-            appBar: AppBar(
-              title: Text("My App Bar", style: TextStyle(color: Colors.white)),
-              backgroundColor: Colors.deepPurple,
-              centerTitle: true,
-              leading: Icon(Icons.menu, color: Colors.white),
-              actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.logout, color: Colors.white),
-                ),
-              ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title: Text("My App Bar", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.deepPurple,
+          centerTitle: true,
+          leading: Icon(Icons.menu, color: Colors.white),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.logout, color: Colors.white),
             ),
-            // body: Center(
-            //   child: Container(
-            //     height: 300,
-            //     width: 300,
-            //     padding: EdgeInsets.all(25),
-            //     decoration: BoxDecoration(
-            //       color: Colors.deepPurple,
-            //       borderRadius: BorderRadius.circular(20),
-            //     ),
-            //     child: Icon(
-            //       Icons.favorite,
-            //       size: 100,
-            //       color: Colors.white,
-            //     ),
-            //   ),
-            // ),
-            body: Column(
-              children: [
-                Container(
-                  height: 200,
-                  width: 100,
-                  color: Colors.deepPurple,
-                ),
-                Container(height: 200, width: 100, color: Colors.deepPurple[100]),
-                Expanded(child: Container(height: 200, width: 100, color: Colors.deepPurple[300]))
-              ],
-            )));
+          ],
+          elevation: 5,
+          shadowColor: Colors.deepPurple[500],
+        ),
+        // body: Center(
+        //   child: Container(
+        //     height: 300,
+        //     width: 300,
+        //     padding: EdgeInsets.all(25),
+        //     decoration: BoxDecoration(
+        //       color: Colors.deepPurple,
+        //       borderRadius: BorderRadius.circular(20),
+        //     ),
+        //     child: Icon(
+        //       Icons.favorite,
+        //       size: 100,
+        //       color: Colors.white,
+        //     ),
+        //   ),
+        // ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple[100],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple[300],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
