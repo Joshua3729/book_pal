0439b11282819c80a27c7224755cad161c0a665d
Joshua Khumalo <3729344@myuwc.ac.za>
2023-12-26 22:31:22 +0200

    adding expanded widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
 
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.deepPurple[200],
+            appBar: AppBar(
+              title: Text("My App Bar", style: TextStyle(color: Colors.white)),
+              backgroundColor: Colors.deepPurple,
+              centerTitle: true,
+              leading: Icon(Icons.menu, color: Colors.white),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.logout, color: Colors.white),
+                ),
+              ],
+            ),
+            // body: Center(
+            //   child: Container(
+            //     height: 300,
+            //     width: 300,
+            //     padding: EdgeInsets.all(25),
+            //     decoration: BoxDecoration(
+            //       color: Colors.deepPurple,
+            //       borderRadius: BorderRadius.circular(20),
+            //     ),
+            //     child: Icon(
+            //       Icons.favorite,
+            //       size: 100,
+            //       color: Colors.white,
+            //     ),
+            //   ),
+            // ),
+            body: Column(
+              children: [
+                Container(
+                  height: 200,
+                  width: 100,
+                  color: Colors.deepPurple,
+                ),
+                Container(height: 200, width: 100, color: Colors.deepPurple[100]),
+                Expanded(child: Container(height: 200, width: 100, color: Colors.deepPurple[300]))
+              ],
+            )));
+  }
+}
